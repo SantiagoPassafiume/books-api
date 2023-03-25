3ef81310eb0f33293bb86b51006ca1646cc61066
Santiago Passafiume <santiagopassafiume@protonmail.com>
2023-03-25 14:05:08 -0300

    fix indentation

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 type Book struct {
-    Title       string `json:"title"`
+    Title       string	`json:"title"`
     Author      string	`json:"author"`
     Publisher   string	`json:"publisher"`
     PublishYear int	`json:"publishYear"`
